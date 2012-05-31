82d815e4140452036b8b7a4c952c43eb2c5e7a28
Mohamed Sanaulla <sanaulla123@gmail.com>
2012-05-31 10:45:07 +0530

    rename the zul file, set the default location for map

--- src/main/java/net/javabeat/MarathonHomeComposer.java
+++ src/main/java/net/javabeat/MarathonHomeComposer.java
@@ -12,22 +12,39 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Div;
 
-public class BorderLayoutComposer extends GenericForwardComposer{
+public class MarathonHomeComposer extends GenericForwardComposer{
 	
 	MapModel mapModel;
 	Gmaps eventsMap;
 	Div contentDiv;
+	List<Gmarker> marathonEvents;
 	
-	public BorderLayoutComposer() throws MalformedURLException, IOException{
+	public MarathonHomeComposer() 
+			throws MalformedURLException, IOException{
 		String marathonEventScheduleSource = "http://runinfinity.com/calendar/india-marathon-calendar";
 		MarathonEventsSource marathonEventsSource = new MarathonEventsSource();
-		List<Gmarker> marathonEvents = marathonEventsSource.getMarathonEvents();	
+		marathonEvents = marathonEventsSource.getMarathonEvents();	
 		mapModel = new MapModelList(marathonEvents);
 		
 	}
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);		
 		eventsMap.setModel(mapModel);
-		eventsMap.setZoom(9);
+		eventsMap.setZoom(5);
+		setTheDefaultLocationOfMap();
+	}
+	
+	/**
+	 * Setting the default location of the map.
+	 * Note: Ideally this should get the user's location i.e IP address
+	 * and get the corresponding Latitude and Longitude values. 
+	 * For now defaulting to setting the location to the location of the first event.
+	 */
+	public void setTheDefaultLocationOfMap(){
+		if ( marathonEvents.size() > 0){
+			Gmarker firstEvent = marathonEvents.get(0);
+			eventsMap.setLat(firstEvent.getLat());
+			eventsMap.setLng(firstEvent.getLng());
+		}
 	}
 }
